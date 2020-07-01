88a732729ff7016389a8dad4aad918e0cc21e425
荀徒之 <kalxd@126.com>
2020-07-01 09:41:02 +0800

    匹配不同类型文件。

--- aipom/server.rkt
+++ aipom/server.rkt
@@ -6,20 +6,28 @@
          racket/list
          racket/string
          racket/file
+         racket/path
+         racket/match
 
          "./flag.rkt")
 
 (provide 启动服务)
 
-(define RESPONSE-TYPE
-  #"text/plain; charset=utf-8")
+;;; 匹配MIME类型。
+(define (匹配MINE 文件扩展)
+  (define t (match 文件扩展
+              [#".html" #"text/html"]
+              [#".css" #"text/css"]
+              [#".js" #"text/javascript"]
+              [else #"text/plain"]))
+  (bytes-append t #"; charset=utf-8"))
 
 ;;; 检测文件是否存在，
 ;;; 不存在则尝试读取index.html。
 (define (检测文件 文件)
   (if (file-exists? 文件)
       文件
-      (let ([索引文件 (build-path 文件 'up "index.html")])
+      (let ([索引文件 (build-path 文件 'same "index.html")])
         (and (file-exists? 索引文件) 索引文件))))
 
 (define (拼接请求路径 目录 req)
@@ -28,19 +36,20 @@
     (apply build-path (cons 目录 path))))
 
 (define (输出请求文件 文件)
-  (define body (file->bytes 文件))
-  (response/full 200
-                 #"OK"
-                 (current-seconds)
-                 RESPONSE-TYPE
-                 empty
-                 (list body)))
+  (let ([mine (匹配MINE (path-get-extension 文件))]
+        [body (file->bytes 文件)])
+    (response/full 200
+                   #"OK"
+                   (current-seconds)
+                   mine
+                   empty
+                   (list body))))
 
 (define 输出未找到
   (response/full 404
                  #"Not Found"
                  (current-seconds)
-                 RESPONSE-TYPE
+                 #"text/html; charset=utf-8"
                  empty
                  empty))
 
@@ -52,6 +61,7 @@
         输出未找到)))
 
 (define (启动服务 标识)
+  (displayln 标识)
   (let ([端口 (命令标识体-端口 标识)]
         [目录 (命令标识体-目录 标识)])
     (serve/servlet (λ (req) (请求处理 目录 req))
